3534c291a93e1624892be49f6dcb5d742d9cf880
james-lafferty <james@pretty-code.com>
2025-09-24 17:52:08 -0700

    feat(documentation): :sparkles: Add docusaurus skeleton site

---
+++ apps/documentation/docusaurus.config.ts
@@ -0,0 +1,55 @@
+import type * as Preset from "@docusaurus/preset-classic"
+import type { Config } from "@docusaurus/types"
+import rehypePlantUML from "@interrogate/rehype-plugin-plantuml"
+import remarkPlantUML from "@interrogate/remark-plugin-plantuml"
+import { themes as prismThemes } from "prism-react-renderer"
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Interrogate",
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+  // Set the production url of your site here
+  url: "https://docs.interrogate.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "interrogate-io", // Usually your GitHub org/user name.
+  projectName: "interrogate", // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  presets: [
+    [
+      "classic",
+      {
+        blog: false,
+        docs: {
+          beforeDefaultRemarkPlugins: [remarkPlantUML],
+          rehypePlugins: [rehypePlantUML],
+          routeBasePath: "/",
+        },
+        pages: false,
+      } satisfies Preset.Options,
+    ],
+  ],
+  themeConfig: {
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+}
+
+export default config
