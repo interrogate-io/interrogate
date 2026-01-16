78c0f5ff5f5c265f4c54a72d3dd4370bd5b8c2fd
james-lafferty <james@pretty-code.com>
2026-01-15 21:11:42 -0800

    test(plantuml-to-svg): :white_check_mark: Improve code coverage

--- libs/plantuml-to-svg/src/lib/plantuml-to-svg.test.ts
+++ libs/plantuml-to-svg/src/lib/plantuml-to-svg.test.ts
@@ -1,9 +1,27 @@
 import { parse, type ElementNode, type Node } from "svg-parser"
-import { describe, expect, it, vi } from "vitest"
+import { optimize } from "svgo"
+import { afterEach, describe, expect, it, vi, type Mock } from "vitest"
+import { getCommand } from "./get-command.ts"
 import { plantUMLToSVG } from "./plantuml-to-svg.ts"
 
+vi.mock("svgo", async importOriginal => {
+  const { optimize, ...rest } = await importOriginal<typeof import("svgo")>()
+  return {
+    ...rest,
+    optimize: vi.fn(optimize),
+  }
+})
+vi.mock("./get-command.js", async importOriginal => {
+  const { getCommand } = await importOriginal<typeof import("./get-command.js")>()
+  return {
+    getCommand: vi.fn(getCommand),
+  }
+})
 const isElementNode = (node: Node): node is ElementNode => node.type === "element"
 describe("plantUMLToSVG", () => {
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
   it("should convert plantuml markup to an svg", async () => {
     const value = await plantUMLToSVG("A -> B: Hello")
     const svg = parse(value).children.find(node => {
@@ -40,16 +58,34 @@ describe("plantUMLToSVG", () => {
     expect(svg?.properties?.width).toBeUndefined()
   })
   it("should throw an error when getCommand does not return", async () => {
-    vi.resetModules()
-    vi.doMock("./get-command.js", () => ({
-      getCommand: vi.fn(() => null),
-    }))
-    const { plantUMLToSVG } = await import("./plantuml-to-svg.ts")
+    ;(getCommand as Mock).mockResolvedValue(null)
     await expect(() => plantUMLToSVG("A -> B: Hello")).rejects.toEqual(
       expect.objectContaining({
         message: "Could not invoke docker or java",
       }),
     )
-    vi.unmock("./get-command.js")
+  })
+  it("should return an svg when the command returns an svg", async () => {
+    ;(getCommand as Mock).mockResolvedValue("./test.sh")
+    const value = await plantUMLToSVG("<svg/>")
+    expect(value).toEqual("<svg/>")
+  })
+  it("should throw an error when the command returns junk", async () => {
+    ;(getCommand as Mock).mockResolvedValue("./test-junk.sh")
+    await expect(() => plantUMLToSVG("A -> B: Hello")).rejects.toEqual(
+      expect.objectContaining({
+        message: "junk\n",
+      }),
+    )
+  })
+  it("should throw an error when svgo fails", async () => {
+    ;(optimize as Mock).mockImplementation(() => {
+      throw new Error("error")
+    })
+    await expect(() => plantUMLToSVG("A -> B: Hello")).rejects.toEqual(
+      expect.objectContaining({
+        message: "error",
+      }),
+    )
   })
 })
